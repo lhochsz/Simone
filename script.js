f77885a485418ec9cd6858f0403dccd88ceb3d13
Leslie Hochsztein <lhochsz@gmail.com>
2016-07-31 18:56:29 -0400

    Got game working

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var game = {
 // Change color if button selected by ID
 
    $(document).ready(function() {
+      var audio = $("#sound");
 
       function animate(divid) {
 
@@ -18,31 +19,40 @@ var game = {
 
       if (divid == "p") {
         $("#p").css("background-color", "#ff00c3");
+        $("#tune").attr("src", "sounds/Jump-SoundBible.com-1007297584.mp3")
         setTimeout(function() {
           $("#p").css("background-color", "#fca9e7"); }, 200);
       } else if (divid == "y") {
         $("#y").css("background-color", "#fff600");
+        $("#tune").attr("src", "sounds/Robot_blip_2-Marianne_Gagnon-299056732.mp3")
         setTimeout(function() {
           $("#y").css("background-color", "#fcf8a9"); }, 200);
       } else if (divid == "w") {
         $("#w").css("background-color", "#aaaaaa");
+        $("#tune").attr("src", "sounds/Robot_blip-Marianne_Gagnon-120342607.mp3")
         setTimeout(function() {
           $("#w").css("background-color", "white"); }, 200);
       } else if (divid == "b") {
         $("#b").css("background-color", "#00ddff");
+        $("#tune").attr("src", "sounds/Ting-Popup_Pixels-349896185.mp3")
         setTimeout(function() {
           $("#b").css("background-color", "#b5eaf2"); }, 200);
     }
-}
+
+
+        audio[0].pause();
+        audio[0].load();
+        audio[0].play();
+    }
 
 // Make ID
 
     function makeId() {
         var text = "";
-        var possibleButton = "pywb";
+        var possibleColor = "pywb";
 
         for (var i = 0; i < 1; i++) {
-          text += possibleButton.charAt(Math.floor(Math.random() * possibleButton.length));
+          text += possibleColor.charAt(Math.floor(Math.random() * possibleColor.length));
           game.sequence.push(text);
         }
 
@@ -58,14 +68,13 @@ var game = {
         }
       }, game.speed)
     }
-
     gameSequence();
 }
 
 // Listen for player click
 
     function listenForClick() {
-      $("#startGameButton").on("mousedown", function() {
+      $("#p, #y, #w, #b").on("mousedown", function() {
           if (this.id == game.sequence[game.clicked]) {
 
               if (game.clicked === game.sequence.length - 1) {
@@ -82,6 +91,8 @@ var game = {
             $("#newGameButton").show();
             $("#startGameButton").hide();
             $("#newGameButton").addClass("slideUp");
+            $("#simon, #count").css("filter", "blur(5px)");
+            $("#simon, #count").css("-webkit-filter", "blur(5px)");
             game.clicked = 0;
             $("#p, #y, #w, #b").off("mousedown");
           }
@@ -91,11 +102,13 @@ var game = {
 // Start game
 
   $("#p, #y, #w, #b").on("click", function() {
-    animate(this.id);
+    animate(this.id)
   });
 
   $("#startGameButton").on("click", function() {
     $("#startGameButton").hide();
+    $("#simon, #count").css("filter", "blur(0px)");
+    $("#simon, #count").css("-webkit-filter", "blur(0px)");;
     game.round++;
     makeId();
     $("#count").html(game.round);
