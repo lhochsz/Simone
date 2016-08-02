71c41da29b2c923645229640e7a740b09afaf381
Leslie Hochsztein <lhochsz@gmail.com>
2016-08-02 15:56:07 -0400

    Got user click sounds to work. Cleaning up CSS

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
 //listen for start button click and then call simon's turn
 
 $("#startGameButton").click(function() {
-  simonsTurn();
+  $("#startGameButton").hide();
+  simonesTurn();
   listenForUserClick();
   $("#headline").html('Simone');
 });
@@ -44,20 +45,26 @@ function animateButtons() {
   }
 }
 
-//set up how to animate
+// set up how to animate and add sound
 function howToAnimate(color) {
   console.log('howToAnimate:', color);
   var simoneButton = $('#' + color);
   simoneButton.addClass('selected');
+    function addSound() {
+      var playSound = $(simoneButton).attr('data-tile');
+      $('#sound' + playSound)[0].play();
+    }
+    addSound();
   setTimeout(function() {
     simoneButton.removeClass('selected');
   }, 320);
+
 }
 
 //it's simon's turn
 // call light up function to highlight a button
 
-function simonsTurn() {
+function simonesTurn() {
   addNewRandomMove();
   animateButtons();
   $("#count").html(game.sequence.length);
@@ -85,7 +92,7 @@ function listenForUserClick() {
           if (game.userClicks === game.sequence.length) {
             game.userClicks = 0;
             setTimeout(function() {
-              simonsTurn();
+              simonesTurn();
               }, 500);
           }
     } else {
@@ -93,41 +100,57 @@ function listenForUserClick() {
           $("#headline").text("Well, you failed!");
           endOfGame();
           // disable Simone buttons
+          $("#startGameButton").show();
           $("#startGameButton").text("New Game?");
     }
   });
 }
 
- $("#p").mousedown(function() {
+ $("#p").click(function() {
     $("#p").addClass('selected');
+    pAudio.play();
      setTimeout(function() {
       $("#p").removeClass('selected');
     }, 320);
   });
 
-  $("#y").mousedown(function() {
+  $("#y").click(function() {
     $("#y").addClass('selected');
+    yAudio.play();
      setTimeout(function() {
       $("#y").removeClass('selected');
     }, 320);
+
   });
 
-   $("#w").mousedown(function() {
+   $("#w").click(function() {
     $("#w").addClass('selected');
+    wAudio.play();
      setTimeout(function() {
       $("#w").removeClass('selected');
     }, 320);
   });
 
-  $("#b").mousedown(function() {
+  $("#b").click(function() {
     $("#b").addClass('selected');
+    bAudio.play();
      setTimeout(function() {
       $("#b").removeClass('selected');
     }, 320);
   });
 
+var pAudio = document.createElement('audio');
+  pAudio.setAttribute('src', 'sounds/Jump-SoundBible.com-1007297584.mp3');
+  pAudio.setAttribute('autoplay:false', 'autoplay');
 
+var yAudio = document.createElement('audio');
+  yAudio.setAttribute('src', 'sounds/Robot_blip_2-Marianne_Gagnon-299056732.mp3');
+  yAudio.setAttribute('autoplay:false', 'autoplay');
 
+var wAudio = document.createElement('audio');
+  wAudio.setAttribute('src', 'sounds/Robot_blip-Marianne_Gagnon-120342607.mp3');
+  wAudio.setAttribute('autoplay:false', 'autoplay');
 
-
-
+var bAudio = document.createElement('audio');
+  bAudio.setAttribute('src', 'sounds/Ting-Popup_Pixels-349896185.mp3');
+  bAudio.setAttribute('autoplay:false', 'autoplay');
